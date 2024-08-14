a27a8860a7000a61c09ec925c6d4719728ee6e02
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-08-15 02:06:32 +0300

    adding tailwind + dynamic rendering modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import AddCompanyButton from './components/add-company-button';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
